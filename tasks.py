a08f855684e081e6641e437d39a94a6d4b87571b
Igor Filatov <siqniq@bk.ru>
2023-05-09 16:18:15 +0300

    print_values_with_even_indexes improve

--- tasks.py
+++ tasks.py
@@ -52,10 +52,10 @@ def print_even_values_from(list_values: list, start_index=0) -> None:
 
 # 6. печать элементов списка с чётными индексами;
 def print_values_with_even_indexes(list_values: list, start_index=0) -> None:
+    start_index = start_index if (start_index % 2 == 0) else start_index + 1
+
     if start_index >= len(list_values):
         return
-
-    start_index = start_index if (start_index % 2 == 0) else start_index + 1
 
     print(list_values[start_index])
     print_values_with_even_indexes(list_values, start_index + 2)
